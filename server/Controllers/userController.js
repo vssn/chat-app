15f7c97ed2004159a84262291b7d84debb2111dc
Edmund <matrim.eg@gmail.com>
2024-07-09 21:55:44 +0000

    Updated controller, wip

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -1,7 +1,33 @@
 const userModel = require("../Models/userModel.js")
+const bcrypt = require("bcrypt")
+const validator = require("validator")
+const jwt = require("jsonwebtoken")
 
-const registerUser = (req, res) => {
-    res.send("Register2")
+const createToken = (_id) => {
+    const jwtkey =  process.env.JWT_SECRET_KEY;
+
+    return jwt.sign({})
+}
+
+const registerUser = async (req, res) => {
+    const { name, email, password }= req.body
+
+    let user = await userModel.findOne({email})
+
+    if(user) return res.status(400).json("User with the given email already exists.")
+
+    if(!name || !email || !password) return res.status(400).json("All fields are required")
+
+    if(!validator.isEmail(email)) return res.status(400).json("E-Mail must be a valid e-mail.")
+    if(!validator.isStrongPassword(password)) return res.status(400).json("Password must be a strong password.")
+
+    user = new userModel({name, email, password})
+
+    const salt = await bcrypt.genSalt(10)
+
+    user.password = await bcrypt.hash(user.password, salt)
+
+    await user.save()
 }
 
 module.exports = {
