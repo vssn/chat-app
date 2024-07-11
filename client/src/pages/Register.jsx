5a6c8af62246077d9d6d0d3a0fc17667ecf179e2
Edmund <matrim.eg@gmail.com>
2024-07-11 22:11:58 +0000

    AuthContext setup

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,6 +1,10 @@
+import { useContext } from "react";
 import { Alert, Button, Form, Row, Col, Stack } from "react-bootstrap";
+import { AuthContext } from "../context/AuthContext";
 
 const Register = () => {
+  const {registerInfo, updateRegisterInfo} = useContext(AuthContext);
+
   return (
     <>
       <Form>
@@ -15,9 +19,9 @@ const Register = () => {
             <Stack gap={3}>
               <h2>Register</h2>
 
-              <Form.Control type="text" placeholder="Name" />
-              <Form.Control type="email" placeholder="Email" />
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control type="text" placeholder="Name" onChange={(e) => updateRegisterInfo({...registerInfo, name: e.target.value})}/>
+              <Form.Control type="email" placeholder="Email" onChange={(e) => updateRegisterInfo({...registerInfo, email: e.target.value})}/>
+              <Form.Control type="password" placeholder="Password" onChange={(e) => updateRegisterInfo({...registerInfo, password: e.target.value})}/>
 
               <Button variant="primary" type="submit">
                 Register
