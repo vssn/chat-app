108afe09a2f0f782b647a835f858d20bc999f2c9
Edmund <matrim.eg@gmail.com>
2024-07-13 19:54:38 +0000

    server: chat model, controller and routes

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const cors = require("cors");
 const mongoose = require("mongoose");
 const userRoute = require("./Routes/userRoute.js")
+const chatRoute = require("./Routes/chatRoute.js")
 
 const app = express()
 require("dotenv").config()
@@ -15,6 +16,7 @@ app.use(cors({
 }))
 app.use(express.json())
 app.use("/api/users", userRoute)
+app.use("/api/chats", chatRoute)
 
 app.get("/", (req, res) => {
     res.send("Welcome to our chat app API") 
