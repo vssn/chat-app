a7b184049022f5ec750d26605da6317c5fedbef1
Edmund <matrim.eg@gmail.com>
2024-08-14 20:54:03 +0000

    Added README for project

---
+++ README.md
@@ -0,0 +1,7 @@
+
+# Chat App 
+## based on the youtube tutoral of Chaoo Charles: https://www.youtube.com/@ChaooCharles
+
+### Instructions:
+Run the projects in each folder with npm run dev
+
--- socket/index.js
+++ socket/index.js
@@ -1,10 +1,22 @@
 const { Server } = require("socket.io")
-const io = new Server({ cors: {
-    origin: "*"
-} });
+const io = new Server({
+    cors: {
+        origin: "*"
+    }
+});
+
+let onlineUsers = []
 
 io.on("connection", socket => {
     console.log("new connection", socket.id)
+
+    // listen to a connection
+    socket.on("addNewUser", (userId) => {
+        onlineUsers.push({
+            userId,
+            socketId: socket.id
+        })
+    })
 })
 
 io.listen(3000)
